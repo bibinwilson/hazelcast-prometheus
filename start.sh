a11f704e9de00c0ba6294ba248ab594c6e244b25
Bibin <bibin@jarvis.local>
2017-10-11 17:21:13 +0530

    update

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 PRG="$0"
 PRGDIR=`dirname "$PRG"`
 HAZELCAST_HOME=`cd "$PRGDIR/.." >/dev/null; pwd`/hazelcast
+JAVA_OPTS="-javaagent:/opt/jmx_prometheus_javaagent.jar=5000:/opt/config.xml"
 
 if [ "x$MIN_HEAP_SIZE" != "x" ]; then
 	JAVA_OPTS="$JAVA_OPTS -Xms${MIN_HEAP_SIZE}"
@@ -20,4 +21,4 @@ echo "# JAVA_OPTS=$JAVA_OPTS"
 echo "# starting now...."
 echo "########################################"
 
-java -javaagent:./opt/jmx_prometheus_javaagent-0.10.jar=5000:/opt/config.yaml -server $JAVA_OPTS -Djava.net.preferIPv4Stack=true com.hazelcast.core.server.StartServer
+java -server $JAVA_OPTS -Djava.net.preferIPv4Stack=true com.hazelcast.core.server.StartServer
